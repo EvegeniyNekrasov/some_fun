93fb81da74e7bbcbee15280bfefaa260c77a94df
EvegeniyNekrasov <eanekrasovart@gmail.com>
2025-05-15 21:23:29 +0200

    feat: adding client

---
+++ client/vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [
+        TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+        react(),
+    ],
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:6969",
+                changeOrigin: true,
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            "@assets": path.resolve(__dirname, "./src/assets"),
+            "@hooks": path.resolve(__dirname, "./src/hooks"),
+            "@utils": path.resolve(__dirname, "./src/utils"),
+            "@types": path.resolve(__dirname, "./src/types"),
+            "@components": path.resolve(__dirname, "./src/components"),
+            "@routes": path.resolve(__dirname, "./src/routes"),
+        },
+    },
+});
